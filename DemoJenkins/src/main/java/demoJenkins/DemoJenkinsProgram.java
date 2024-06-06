499f08dbf3fb2c068210a2eae51e177620102635
Ash <Ash@LAPTOP-S5AQL09G>
2024-06-06 23:16:32 +0530

    commit ok

---
+++ DemoJenkins/src/main/java/demoJenkins/DemoJenkinsProgram.java
@@ -0,0 +1,14 @@
+package demoJenkins;
+
+public class DemoJenkinsProgram {
+	public static void display()
+	{
+		System.out.println("Hello World");
+	}
+
+	public static void main(String[] args) {
+		DemoJenkinsProgram.display();
+
+	}
+
+}
